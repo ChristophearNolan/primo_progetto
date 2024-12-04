eed94b6fa65ab3a65b988bbebeb58fa8e44c7ad6
ChristophearNolan <christian.verga@alessandrinimainardi.edu.it>
2024-12-04 10:13:18 +0100

    modifiche eseguitp

--- primo_progetto/settings.py
+++ primo_progetto/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'prima_app',
+    'seconda_app',
 ]
 
 MIDDLEWARE = [
@@ -55,9 +56,12 @@ ROOT_URLCONF = 'primo_progetto.urls'
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'prima_app/templates')],
-        'APP_DIRS': True,
+         'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR,'prima_app/templates'),
+                 os.path.join(BASE_DIR,'seconda_app/templates'),
+                 os.path.join(BASE_DIR,'templates')],
+                 
+        'APP_DIRS': True,       
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
