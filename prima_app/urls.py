c8497ad720b00179d18a85372ed4754c60469226
ChristophearNolan <christian.verga@alessandrinimainardi.edu.it>
2024-12-04 09:25:42 +0100

    passo 8 modificato
    modifica passo 8

--- prima_app/urls.py
+++ prima_app/urls.py
@@ -1,11 +1,13 @@
 from os import path
 from django.urls import path # type: ignore
-from prima_app.views import homepage, welcome, lista,chiSiamo
+from prima_app.views import homepage, welcome, lista,chiSiamo,variabili,index
 
 app_name="prima_app"
 urlpatterns=[
-    path('',homepage,name='homepage'),
+    path('homepage',homepage,name='homepage'),
      path('welcome',welcome,name='welcome'),
      path('lista',lista,name='lista'),
-     path('chiSiamo',chiSiamo,name='chiSiamo')
+     path('chiSiamo',chiSiamo,name='chiSiamo'),
+     path('variabili',variabili,name='variabili'),
+     path('',index,name='index')
 ]
